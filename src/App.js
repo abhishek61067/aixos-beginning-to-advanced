8cd4609d509ce015698ed676b545c5252ba1e763
abhishek bhattarai <abhishek.bhattarai61067@gmail.com>
2023-11-18 22:25:10 +0545

    chore: add documentation for global defaults and its limitations

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@ import Title from './components/Title';
 import FirstRequest from './examples/1-first-request'
 import HeaderRequest from './examples/2-headers.js'
 import PostRequest from './examples/3-post-request.js'
+import './axios-global-defaults/axios-global-defaults'
+import GlobalDefaults from './examples/4-global-instance'
 function App() {
   return (
     <main>
       <Title />
       {/* <FirstRequest/> */}
 {/* <HeaderRequest/> */}
-<PostRequest/>
+{/* <PostRequest/> */}
+<GlobalDefaults/>
 
     </main>
   );
--- src/axios-global-defaults/axios-global-defaults.js
+++ src/axios-global-defaults/axios-global-defaults.js
@@ -1,3 +1,5 @@
 import axios from "axios";
-
+// For global defaults, we will add those headers and  other options which wont change like
+// authorization and base url which maynot be the same for every axios request for this project
+// For that, axios instance is required
 axios.defaults.headers.common["Accept"] = "application/json";
