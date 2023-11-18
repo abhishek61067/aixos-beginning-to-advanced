bfe0e2cee7f1812085fe4ea2a9bb3b309fb5e22c
abhishek bhattarai <abhishek.bhattarai61067@gmail.com>
2023-11-18 22:26:20 +0545

    fix: correct the comment

--- src/axios-global-defaults/axios-global-defaults.js
+++ src/axios-global-defaults/axios-global-defaults.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-// For global defaults, we will add those headers and  other options which wont change like
+// For global defaults, we will add those headers and  other options which wont change like Accept and not
 // authorization and base url which maynot be the same for every axios request for this project
 // For that, axios instance is required
 axios.defaults.headers.common["Accept"] = "application/json";
