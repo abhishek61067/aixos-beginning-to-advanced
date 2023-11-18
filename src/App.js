782705117aad08ec79a75ffe4e3ba9ae4c9bc842
abhishek bhattarai <abhishek.bhattarai61067@gmail.com>
2023-11-18 21:26:11 +0545

    feat: add post request

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import Title from './components/Title';
+import FirstRequest from './examples/1-first-request'
+import HeaderRequest from './examples/2-headers.js'
+import PostRequest from './examples/3-post-request.js'
 function App() {
   return (
     <main>
       <Title />
+      {/* <FirstRequest/> */}
+{/* <HeaderRequest/> */}
+<PostRequest/>
+
     </main>
   );
 }
--- src/examples/1-first-request.js
+++ src/examples/1-first-request.js
@@ -1,12 +1,23 @@
-import { useEffect } from 'react';
+import { useEffect } from "react";
+import axios from "axios";
 // limit, if 429 wait for 15 min and try again
-const url = 'https://course-api.com/react-store-products';
+const url = "https://course-api.com/react-store-products";
 
 const FirstRequest = () => {
   useEffect(() => {
-    console.log('first axios request');
+    const fetchData = async () => {
+      try {
+        console.log('response: ', await axios(url));
+        const {data} = await axios(url);
+        console.log("data: ", data);
+
+      } catch (e) {
+        console.log("error: ", e.response);
+      }
+    };
+    fetchData();
   }, []);
 
-  return <h2 className='text-center'>first request</h2>;
+  return <h2 className="text-center">first request</h2>;
 };
 export default FirstRequest;
